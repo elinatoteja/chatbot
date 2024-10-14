63bf1cace43bedea3eb7e6d53999102b7ee9f3f9
elinatoteja <elinatoteja@gmail.com>
2024-10-14 01:16:30 +0000

    rag completed

---
+++ rag.py
@@ -0,0 +1,87 @@
+mport os
+import base64
+import requests
+
+import streamlit as st
+from openai import OpenAI
+from langchain_core.prompts import ChatPromptTemplate
+from langchain_core.output_parsers import StrOutputParser
+from langchain_openai import ChatOpenAI
+from pinecone import Pinecone
+# keys
+os.environ["OPENAI_API_KEY"] = st.secrets["OPENAI_API_KEY"]
+os.environ["PINECONE_API_KEY"] = st.secrets["PINECONE_API_KEY"]
+os.environ["INDEX_HOST"] = st.secrets["INDEX_HOST"]
+# constants
+NAMESPACE_KEY = "Elina"
+TEXT_MODEL = "text-embedding-ada-002"
+QA_MODEL = "gpt-4o-mini"
+COMMON_TEMPLATE = """
+"You are a highly trained assisstant supporting child care."
+"Use the following pieces of context to answer the question at the end with human readable answer as a paragraph"
+"Please do not use data outside the context to answer any questions. "
+"If the answer is not in the given context, just say that you don't have enough context."
+"don't try to make up an answer. "
+"\n\n"
+{context}
+"\n\n"
+Question: {question}
+"n"
+"Helpful answer:   "
+"""
+# pinecone setup
+pc = Pinecone(api_key=os.environ["PINECONE_API_KEY"])
+index = pc.Index(host=os.environ["INDEX_HOST"])
+
+# create client
+client = OpenAI(api_key=os.environ["OPENAI_API_KEY"])
+
+
+def get_openai_embeddings(text: str) -> list[float]:
+    response = client.embeddings.create(input=f"{text}", model=TEXT_MODEL)
+
+    return response.data[0].embedding
+    # function query similar chunks
+def query_response(query_embedding, k = 1, namespace_ = NAMESPACE_KEY):
+    query_response = index.query(
+        namespace=namespace_,
+        vector=query_embedding,
+        top_k=k,
+        include_values=False,
+        include_metadata=True,
+    )
+
+    return query_response
+
+
+def content_extractor(similar_data):
+    top_values = similar_data["matches"]
+    # get the text out
+    text_content = [sub_content["metadata"]["text"] for sub_content in top_values]
+    return " ".join(text_content)
+    def get_model():
+    model = ChatOpenAI(model=QA_MODEL, api_key=os.environ["OPENAI_API_KEY"])
+    return model
+
+
+def get_similar_context(question: str):
+    # get the query embeddings
+    quer_embed_data = get_openai_embeddings(question)
+
+    # query the similar chunks
+    similar_chunks = query_response(quer_embed_data)
+
+    # extract the similar text data
+    similar_content = content_extractor(similar_chunks)
+
+    return similar_content
+def streaming_question_answering(query_question: str, context_text: str,  template: str = COMMON_TEMPLATE):
+    prompt = ChatPromptTemplate.from_template(template)
+    model = get_model()
+    output_parser = StrOutputParser()
+
+    # create the chain
+    chain = prompt | model | output_parser
+
+    # get the answer
+    return chain.stream({"context": context_text, "question": query_question})
